4097c309462d34ede035d4805a51e1f80990124d
Yurii <tovy@1c.ru>
2019-10-23 09:53:17 +0300

    Explicitly set up working directory for shell command; add trampoline option for lein

--- reporter.el
+++ reporter.el
@@ -1,14 +1,20 @@
 (eval-when-compile (require 'subr-x))
 
+
+(defconst teamcity-test-reporter/wd
+  (file-name-directory load-file-name))
+
+
 (defun make-teamcity-test-report (args)
   (interactive "sargs: ")
-  (let* ((command (string-join (append '("lein" "run")
+  (let* ((default-directory teamcity-test-reporter/wd)
+         (command (string-join (append '("lein" "trampoline" "run")
                                        (split-string args))
                                " "))
          (report-path (string-trim-right
                        (shell-command-to-string command))))
     (find-file report-path)))
-  
+
 
 (defun make-teamcity-test-report-demo ()
   (interactive)
