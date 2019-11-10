d50d8ed1abef337336159f7b56f216ce554c5e52
Yurii <juretz32@yandex.ru>
2019-11-10 19:12:52 +0300

    Remember last user input

--- reporter.el
+++ reporter.el
@@ -5,8 +5,19 @@
   (file-name-directory load-file-name))
 
 
+(setq teamcity-test-reporter/last-args nil)
+
+
 (defun teamcity-test-report (args)
-  (interactive "sargs: ")
+  (interactive (list (read-from-minibuffer
+                      (if teamcity-test-reporter/last-args
+                          (format "args (default: %s): "
+                                  teamcity-test-reporter/last-args)
+                        "args: "))))
+  (if (zerop (string-width args))
+      (when teamcity-test-reporter/last-args
+        (setq args teamcity-test-reporter/last-args))
+    (setq teamcity-test-reporter/last-args args))
   (let* ((default-directory teamcity-test-reporter/wd)
          (command (string-join (append '("lein" "trampoline" "run")
                                        (split-string args))
